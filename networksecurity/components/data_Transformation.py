85652f8f54fa967b6418c2667628d098ed83e7e6
preeti9311 <174781309+preeti9311@users.noreply.github.com>
2026-08-14 00:25:17 +0530

    transformation

---
+++ networksecurity/components/data_Transformation.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import numpy as np
+import pandas as pd
+from sklearn.impute import KNNImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+
+from networksecurity.constants.training_pipeline import TARGET_COLUMN, DATA_TRANSFORMATION_IMPUTER_PARAMS
+from networksecurity.entity.artifact_entity import DataTransformationArtifact,DataValidationArtifact
+
+from networksecurity.entity.config_entity import DataTransformationConfig
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from networksecurity.utils.main_utils.utils import save_object,save_numpy_array_data
+
+
--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -37,3 +37,15 @@ DATA_VALIDATION_DRIFT_REPORT_DIR : str="drift_report"
 ## schema file path 
 SCHEMA_FILE_PATH =os.path.join("data_schema","schema.yaml")
 
+
+"""
+Constants for the data transformation pipeline
+"""
+DATA_TRANSFORMATION_DIR_NAME : str="data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR : str="transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR : str="preprocessed"
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict={
+    "missing_values":np.nan,
+    "n_neighbors":3,
+    "weights":"uniform",
+}
